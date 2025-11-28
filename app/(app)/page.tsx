d45fe0e7ad73d41ee29ac0adb63314ddc79af7dd
YuliAdam <ylia_adamovich@mail.ru>
2025-11-28 09:50:36 +0100

    feat: add logo and footer

---
+++ app/(app)/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Index() {
+  return (
+    <>
+      <main>Main</main>
+      <Link href="/about">To About</Link>
+    </>
+  );
+}
---
+++ components/footer/Footer.tsx
@@ -0,0 +1,28 @@
+import GitHubLogo from '@/public/github-logo';
+import styles from './footer.module.scss';
+
+const PERSONAL_INFO = {
+  githubLink: 'https://github.com/YuliAdam/Job-Searcher/tree/base-structure',
+  githubName: 'YuliAdam',
+  year: '/2025',
+};
+
+export default function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.wrap}>
+        <a href={PERSONAL_INFO.githubLink} target="_blank">
+          <GitHubLogo className={styles.info_logo} />
+        </a>
+        <a
+          className={styles.info_link}
+          href={PERSONAL_INFO.githubLink}
+          target="_blank"
+        >
+          {PERSONAL_INFO.githubName}
+        </a>
+        <span className={styles.info}>{PERSONAL_INFO.year}</span>
+      </div>
+    </footer>
+  );
+}
