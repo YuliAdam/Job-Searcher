20fbbfdc79c4a8332838679d999737249f83af2e
YuliAdam <ylia_adamovich@mail.ru>
2025-12-15 23:45:30 +0100

    fix: vercel reloading

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,11 +1,10 @@
 import styles from './main.module.scss';
-import CardListWrapper from '@components/cards/CardListWrapper';
+import Pagination from '@components/pagination/Pagination';
 
 export default async function Main() {
-  console.log('main component')
   return (
     <section className={styles.main}>
-      <CardListWrapper />
+      <Pagination />
     </section>
   );
 }
