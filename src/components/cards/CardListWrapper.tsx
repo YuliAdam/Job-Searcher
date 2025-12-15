2a66fae32cfd14db4b4d91a9325a13f838c2c802
YuliAdam <ylia_adamovich@mail.ru>
2025-12-16 00:02:01 +0100

    fix: vercel reloading

--- src/components/cards/CardListWrapper.tsx
+++ src/components/cards/CardListWrapper.tsx
@@ -2,7 +2,7 @@ import { getAllJob } from '@api/apiRequests';
 import type { GetJobResponse } from '@components/interfaces';
 
 export default async function CardListWrapper() {
-  const response: GetJobResponse = await (await getAllJob()).json();
+  const response: GetJobResponse | undefined = await (await getAllJob()).json();
 
-  return <div>{response.jobs.toString()}</div>;
+  return <div>{response ? response.jobCount : 'undef'}</div>;
 }
