478d73c58c6d89a6d2f0fc16258849ba61e9a4a0
YuliAdam <ylia_adamovich@mail.ru>
2025-12-16 00:04:15 +0100

    fix: vercel reloading

--- src/api/apiRequests.ts
+++ src/api/apiRequests.ts
@@ -1,10 +1,6 @@
-import { baseParams, EXTERNAL_LINKS, MAX_JOB_COUNT } from '@config/apiConfig';
+import { EXTERNAL_LINKS} from '@config/apiConfig';
 
 export function getAllJob() {
-  console.log(EXTERNAL_LINKS.baseUrl);
   return EXTERNAL_LINKS.baseUrl
-    ? fetch(
-        EXTERNAL_LINKS.baseUrl.concat(`?${baseParams.count}=${MAX_JOB_COUNT}`)
-      )
-    : Promise.reject('Base link error');
+   
 }
--- src/components/cards/CardListWrapper.tsx
+++ src/components/cards/CardListWrapper.tsx
@@ -1,8 +1,7 @@
 import { getAllJob } from '@api/apiRequests';
-import type { GetJobResponse } from '@components/interfaces';
 
 export default async function CardListWrapper() {
-  const response: GetJobResponse | undefined = await (await getAllJob()).json();
+  const response = getAllJob()
 
-  return <div>{response ? response.jobCount : 'undef'}</div>;
+  return <div>{response ? response : 'undef'}</div>;
 }
