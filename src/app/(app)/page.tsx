254419409d7e54652eeb70919b0da2a02f22c9d2
YuliAdam <ylia_adamovich@mail.ru>
2025-12-15 22:54:25 +0100

    fix: redirect about

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -2,5 +2,5 @@ import { PATHS } from '@config/navigationConfig';
 import { redirect } from 'next/navigation';
 
 export default function RedirectPage() {
-  redirect(PATHS.firstPage);
+  redirect(PATHS.about);
 }
