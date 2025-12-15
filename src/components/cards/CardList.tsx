4b28e971a1554276befe5dd23464614e5d005ccc
YuliAdam <ylia_adamovich@mail.ru>
2025-12-16 00:00:02 +0100

    fix: vercel reloading

--- src/components/cards/CardList.tsx
+++ src/components/cards/CardList.tsx
@@ -1,6 +1,11 @@
 'use client';
 import type { GetJobResponse } from '@components/interfaces';
+import Card from './Card';
 import styles from './card.module.scss';
+import { useDispatch, useSelector } from 'react-redux';
+import { setAllJob } from 'store/slices/jobSlice';
+import { pageSelector } from 'store/selectors';
+import { useEffect } from 'react';
 
 interface Props {
   response: GetJobResponse;
@@ -9,10 +14,18 @@ interface Props {
 export const CARDS_AT_PAGE = 12;
 
 export default function CardList(props: Props) {
- 
+  const page = useSelector(pageSelector);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(setAllJob(props.response.jobs));
+  }, []);
   return (
     <section className={styles.list}>
-    {props.response.jobs.toString()}
+      {props.response.jobs.map(
+        (i, k) =>
+          k >= CARDS_AT_PAGE * (page - 1) &&
+          k < CARDS_AT_PAGE * page && <Card job={i} key={i.id} />
+      )}
     </section>
   );
 }
--- src/components/cards/CardListWrapper.tsx
+++ src/components/cards/CardListWrapper.tsx
@@ -1,8 +1,8 @@
 import { getAllJob } from '@api/apiRequests';
 import type { GetJobResponse } from '@components/interfaces';
-import CardList from './CardList';
 
 export default async function CardListWrapper() {
   const response: GetJobResponse = await (await getAllJob()).json();
-  return <CardList response={response} />;
+
+  return <div>{response.jobs.toString()}</div>;
 }
